bd494fa6884d3328c946a26b1f694cf6cbea15a2
StefaniaKoulouri <t8230063@aueb.gr>
2024-11-14 23:03:45 +0200

    Update AMexercise.java

--- AMexercise.java
+++ AMexercise.java
@@ -2,5 +2,6 @@ public class AMexercise {
     public static void main(String[] args) {
         System.out.println("8230122");
         System.out.println("8230023");
+        System.out.println("8230063");
     }
 }
